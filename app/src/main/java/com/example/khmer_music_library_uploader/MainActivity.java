0a3b8463bba571410e9dbaef94bba695b49fb341
Lin Chhay <chhaylin2@gmail.com>
2020-04-26 23:06:36 +0700

    first commit

---
+++ app/src/main/java/com/example/khmer_music_library_uploader/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.khmer_music_library_uploader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
