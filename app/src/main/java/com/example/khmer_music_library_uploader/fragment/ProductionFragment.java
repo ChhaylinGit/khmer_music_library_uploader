413bc07e349e72a2644036023b668c6b6b57700c
unknown <chhaylin2@gmail.com>
2020-04-30 17:31:00 +0700

    30

--- app/src/main/java/com/example/khmer_music_library_uploader/fragment/ProductionFragment.java
+++ app/src/main/java/com/example/khmer_music_library_uploader/fragment/ProductionFragment.java
@@ -1,7 +1,15 @@
 package com.example.khmer_music_library_uploader.fragment;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,21 +17,70 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.khmer_music_library_uploader.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductionFragment extends Fragment {
 
+    private CircularImageView imageProduction;
+    private FloatingActionButton browseImageProduction;
+    private Uri selectedImage;
+    private static final int RESULT_LOAD_IMAGE = 1;
+
     public ProductionFragment() {
         // Required empty public constructor
     }
 
+    public void permissionsCheck() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, RESULT_LOAD_IMAGE);
+            return;
+        }
+    }
+
+    private void browseImage()
+    {
+        permissionsCheck();
+        Intent intent;
+        if (Build.VERSION.SDK_INT < 19) {
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+        } else {
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+        }
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), RESULT_LOAD_IMAGE);
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_production, container, false);
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            selectedImage = data.getData();
+            imageProduction.setImageURI(selectedImage);
+            imageProduction.invalidate();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_production, container, false);
+        imageProduction= view.findViewById(R.id.imageProduction);
+        browseImageProduction = view.findViewById(R.id.browseImageProduction);
+        browseImageProduction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                browseImage();
+            }
+        });
+        return  view;
     }
 }
